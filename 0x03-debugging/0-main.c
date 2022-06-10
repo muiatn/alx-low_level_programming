5d6a648e2009bca14223317df8a983d3cbccfae5
muiatn <muiatitus80@gmail.com>
2022-06-09 18:40:20 -0700

    sky

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - test functions that print if integeris positive or negative
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
