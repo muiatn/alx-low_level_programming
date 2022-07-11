c89c9199575afefe4bcc1ef033c406a0586f774e
muiatn <muiatitus80@gmail.com>
2022-07-11 03:51:42 -0700

    commit 0

--- 0x0C-more_malloc_free/README.md
+++ 0x0C-more_malloc_free/README.md
@@ -1,4 +1,1 @@
-0. Trust no one
-1. string_nconcat
-2._calloc
-3._array_range
+My more_malloc_free readme
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,37 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
-
-/**
- * Auth: Gomina Maryam
- * Desc: Header file containing prototypes for all functions
- *       used in the 0x0B-more_malloc_free directory.
- */
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
 int _putchar(char c);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
-void *_calloc(unsigned int nmemb, unsigned int size);
+void *_calloc(unsigned int nmedb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-
 #endif
-
-[0]
-#include "main.h"
-#include <stdlib.h>
-
-/**
- * malloc_checked - Allocates memory using malloc.
- * @b: The number of bytes to be allocated.
- *
- * Return: A pointer to the allocated memory.
- */
-void *malloc_checked(unsigned int b)
-{
-	void *mem = malloc(b);
-
-	if (mem == NULL)
-		exit(98);
-
-	return (mem);
-
